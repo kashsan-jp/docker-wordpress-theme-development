e0f8f59f4c63bf935aa58d556006b2f0384cec8d
Kashsan <kashsan_jp@yahoo.co.jp>
2025-01-19 15:55:10 +0900

    2025-1-19-16:00

--- wp-content/themes/demo/functions.php
+++ wp-content/themes/demo/functions.php
@@ -1,9 +1,16 @@
 <?php 
-    add_action( "wp_body_open", function(){
-        ?>
-            <script>
-                alert("OK")
-            </script>
-        <?php
-    })
+   
+    // echo "<pre>";
+    // print_r(get_template_directory_uri());
+    // wp_die();
+
+    function my_script() {
+        wp_enqueue_style( 
+            "stylesheet", 
+            get_template_directory_uri() . "/style.css", 
+            [],
+            "1.0.0",
+            "all");
+    }
+    add_action( "wp_enqueue_script", "my_script");
 ?>
